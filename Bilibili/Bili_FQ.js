ea90875ac7c8398b24f4bd2e6b7bb180d98fa7c5
linuszlx <linuszhang@126.com>
2022-02-21 10:10:22 +0800

    Create Bili_FQ.js

---
+++ Bilibili/Bili_FQ.js
@@ -0,0 +1,235 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data = [
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/6f629bd0dcd71d7b9911803f8e4f94fd0e5b4bfd.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/pgc\/partition_page?page_name=bangumi-operation&title=%E7%95%AA%E5%89%A7&select_id=1",
+      "goto" : "",
+      "param" : "",
+      "type" : 1,
+      "tid" : 13,
+      "name" : "番剧"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/1586ec926eac1ea876cb74d32df51394d8e72341.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/pgc\/partition_page?page_name=gc-operation&title=%E5%9B%BD%E5%88%9B&select_id=1",
+      "goto" : "",
+      "param" : "",
+      "type" : 1,
+      "tid" : 167,
+      "name" : "国创"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/3dfba664353bb2349917eaf81b60db34b2d4c61a.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/pgc\/cinema",
+      "goto" : "",
+      "param" : "",
+      "type" : 1,
+      "tid" : 65545,
+      "name" : "放映厅"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/884a644c6bb4b8bb16f9746ef35fbaba396e0b8c.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/pgc\/partition_page?page_name=documentary-operation&title=%E7%BA%AA%E5%BD%95%E7%89%87&select_id=1",
+      "goto" : "",
+      "param" : "",
+      "type" : 1,
+      "tid" : 177,
+      "name" : "纪录片"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/137edde9deb7dfcdf610ed2d1ec63bae6ef3ba0a.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/pgc\/page\/operation_list?page_id=movie-operation&title=%E7%94%B5%E5%BD%B1",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 23,
+      "name" : "电影"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/30779a6904875754762e666b7076014528ef4834.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/pgc\/partition_page?page_name=tv-operation&title=%E7%94%B5%E8%A7%86%E5%89%A7&select_id=1",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 11,
+      "name" : "电视剧"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/9b3bb8cfc8d87809ffa409bc65def8d8c3eaf72b.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/1",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 1,
+      "name" : "动画"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/3a99c51d00038ced3989686b6f3c49d01aa34207.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/3",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 3,
+      "name" : "音乐"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/4769a6faa9ccfde4a029eca36b979bac486afd14.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/129",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 129,
+      "name" : "舞蹈"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/9c88ce1adaecf31e27121bdbb5a29824d655d0a6.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/4",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 4,
+      "name" : "游戏"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/d5bb279936dbe661f958683231566214056987b2.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/36",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 36,
+      "name" : "知识"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/feed-admin\/4a687a86b49feea68d423fd1bf2c461acfe59b70.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/188",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 188,
+      "name" : "科技"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/feed-admin\/1515d944550494abf81b552a84484dce80287242.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/223",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 223,
+      "name" : "汽车"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/50731fc4b9ec487ef2e3861a97e0eb4671b7bcef.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/160",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 160,
+      "name" : "生活"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/0f5e21f08616f9c02d706433ba1c00bd5b889c7b.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/211",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 211,
+      "name" : "美食"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/9f3303b20e12ac874c379da09bca9ce4d0b2f88c.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/217",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 217,
+      "name" : "动物圈"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/de50290b11c65108eb70766fa887032b948d2e4b.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/119",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 119,
+      "name" : "鬼畜"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/1842562be5ded346d79312b24fafedbc1d78c8e2.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/155",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 155,
+      "name" : "时尚"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/a9bcb4cb7e216c2ea28ba3dc10acd2d210f739bd.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/5",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 5,
+      "name" : "娱乐"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/f90bb1ef59630ad9765486c6088a4944b96e88a3.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/region\/181",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 181,
+      "name" : "影视"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/34f46c749054b1c3c157b0c1c09a5ef2b3539204.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/rank\/",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 65552,
+      "name" : "全区排行榜"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/a93687a7f29da88ee375109389b0634412847bd1.png",
+      "reid" : 0,
+      "uri" : "https:\/\/www.bilibili.com\/h5\/game\/home?navhide=1",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 65550,
+      "name" : "游戏赛事"
+    },
+    {
+      "logo" : "http:\/\/i0.hdslb.com\/bfs\/archive\/85d6dddbdc9746fed91c65c2c3eb3a0a453eadaf.png",
+      "reid" : 0,
+      "uri" : "bilibili:\/\/music\/home?from=category",
+      "goto" : "",
+      "param" : "",
+      "type" : 0,
+      "tid" : 65543,
+      "name" : "音频"
+    }
+  ];
+$done({body: JSON.stringify(obj)});
