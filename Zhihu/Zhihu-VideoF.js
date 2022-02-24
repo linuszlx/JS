f46064dd55a4390067aed6c3ece674d14a49626e
linuszlx <linuszhang@126.com>
2022-02-24 22:07:29 +0800

    Update Zhihu-VideoF.js

--- Zhihu/Zhihu-VideoF.js
+++ Zhihu/Zhihu-VideoF.js
@@ -1,6 +1,353 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 obj.data = [
+      {
+      "author" : {
+        "id" : "",
+        "description" : "",
+        "is_advertiser" : false,
+        "uid" : "",
+        "avatar_url_template" : "",
+        "headline" : "",
+        "gender" : 1,
+        "avatar_url" : "",
+        "url" : "",
+        "is_following" : false,
+        "type" : "people",
+        "badge" : [
+          {
+            "type" : "identity",
+            "description" : "影视领域答主",
+            "topics" : null
+          }
+        ],
+        "badge_v2" : {
+          "merged_badges" : [
+            {
+              "night_icon" : "",
+              "title" : "认证",
+              "sources" : [
+
+              ],
+              "detail_type" : "identity",
+              "type" : "identity",
+              "description" : "",
+              "icon" : "",
+              "url" : "https:\/\/www.zhihu.com\/verify\/info"
+            }
+          ],
+          "night_icon" : "",
+          "title" : "影视领域答主",
+          "detail_badges" : [
+            {
+              "night_icon" : "",
+              "title" : "已认证的个人",
+              "sources" : [
+
+              ],
+              "detail_type" : "identity_people",
+              "type" : "identity",
+              "description" : "",
+              "icon" : "",
+              "url" : "https:\/\/www.zhihu.com\/verify\/info"
+            }
+          ],
+          "icon" : ""
+        },
+        "url_token" : "",
+        "is_org" : false,
+        "is_followed" : false,
+        "name" : "",
+        "user_type" : "people"
+      },
+      "contribute" : {
+        "latest_contribute" : {
+          "end_time" : 113085,
+          "target" : {
+            "parent_object" : {
+              "answer_count" : ,
+              "id" : "",
+              "title" : "",
+              "type" : "question",
+              "follow_count" : 87004
+            },
+            "object" : {
+              "type" : "answer",
+              "id" : ""
+            }
+          },
+          "status" : 2,
+          "id" : "",
+          "image_url" : "",
+          "start_time" : 0,
+          "is_complete_video" : true,
+          "duration_millis" : ,
+          "sub_video_id" : ""
+        },
+        "unfinished_count" : 0,
+        "visitor_circulate_count" : 3,
+        "totals" : 3,
+        "contribute_list" : [
+
+        ],
+        "has_new_fail" : false
+      },
+      "id" : "",
+      "published_at" : ,
+      "can_comment" : {
+        "status" : true,
+        "reason" : ""
+      },
+      "is_liked" : false,
+      "is_update_reviewing" : false,
+      "zvideo_collections" : [
+        {
+          "image_path" : "",
+          "zvideo_count" : 13,
+          "author" : {
+            "url_token" : "",
+            "is_org" : false,
+            "url" : "",
+            "id" : "",
+            "is_following" : false,
+            "actived_at" : ,
+            "use_default_avatar" : false,
+            "avatar_url_template" : "",
+            "type" : "people",
+            "name" : "",
+            "avatar_url" : ""
+          },
+          "id" : "",
+          "collection_id" : "",
+          "created_at" : ,
+          "voteup_count" : 3256,
+          "description" : "",
+          "name" : ""
+        }
+      ],
+      "url" : "",
+      "description" : "",
+      "favlists_count" : 1049,
+      "share_count" : 2745,
+      "play_count" : 3737250,
+      "image_url" : "",
+      "is_favorited" : false,
+      "type" : "zvideo",
+      "friend_interaction" : {
+        "upvote_cnt" : 0,
+        "upvote_members" : [
+
+        ]
+      },
+      "campaign" : {
+        "html_description" : "",
+        "id" : "",
+        "head_image" : "",
+        "title" : "",
+        "excerpt_title" : "",
+        "description" : "",
+        "type" : ""
+      },
+      "is_visible" : true,
+      "reactions" : {
+        "LIKE" : {
+          "count" : 3072,
+          "reacted" : false,
+          "reaction_type" : "LIKE"
+        },
+        "VOTE" : {
+          "reacted" : false,
+          "options" : {
+            "UP" : {
+              "count" : 3457
+            },
+            "DOWN" : {
+
+            }
+          },
+          "reaction_type" : "VOTE"
+        }
+      },
+      "attached_info" : "",
+      "mcn_linkcards" : null,
+      "comment_count" : 218,
+      "bullet_count" : 0,
+      "voting" : 0,
+      "is_labeled" : false,
+      "has_column" : false,
+      "content_id" : "",
+      "comment_permission" : "all",
+      "video" : {
+        "status" : "success",
+        "components" : [
+          {
+            "type" : "follow",
+            "content" : {
+              "user" : {
+                "id" : "",
+                "description" : "",
+                "is_advertiser" : false,
+                "uid" : "1167946823326552064",
+                "avatar_url_template" : "",
+                "headline" : "",
+                "gender" : 1,
+                "avatar_url" : "",
+                "url" : "",
+                "is_following" : false,
+                "type" : "people",
+                "badge" : [
+                  {
+                    "type" : "identity",
+                    "description" : "影视领域答主",
+                    "topics" : null
+                  }
+                ],
+                "badge_v2" : {
+                  "merged_badges" : [
+                    {
+                      "night_icon" : "",
+                      "title" : "认证",
+                      "sources" : [
+
+                      ],
+                      "detail_type" : "identity",
+                      "type" : "identity",
+                      "description" : "",
+                      "icon" : "",
+                      "url" : "https:\/\/www.zhihu.com\/verify\/info"
+                    }
+                  ],
+                  "night_icon" : "",
+                  "title" : "影视领域答主",
+                  "detail_badges" : [
+                    {
+                      "night_icon" : "",
+                      "title" : "已认证的个人",
+                      "sources" : [
+
+                      ],
+                      "detail_type" : "identity_people",
+                      "type" : "identity",
+                      "description" : "",
+                      "icon" : "",
+                      "url" : "https:\/\/www.zhihu.com\/verify\/info"
+                    }
+                  ],
+                  "icon" : ""
+                },
+                "url_token" : "",
+                "is_org" : false,
+                "is_followed" : false,
+                "name" : "",
+                "user_type" : ""
+              }
+            },
+            "trigger_condition" : {
+              "compact" : {
+                "type" : "elapsed",
+                "condition" : {
+                  "elasped_millis" : 6000,
+                  "duration_millis" : 3000
+                }
+              },
+              "full" : {
+                "type" : "elapsed",
+                "condition" : {
+                  "elasped_millis" : 6000,
+                  "duration_millis" : 3000
+                }
+              }
+            }
+          }
+        ],
+        "video_id" : "",
+        "playlist_v2" : {
+          "fhd" : {
+            "format" : "mp4",
+            "height" : 1080,
+            "channels" : 2,
+            "play_url" : "",
+            "fps" : ,
+            "duration" : ,
+            "size" : ,
+            "width" : 1920,
+            "bitrate" : 644,
+            "sample_rate" : 44100,
+            "url" : ""
+          },
+        "begin_frame" : {
+          "FHD" : "",
+        },
+        "width" : 1920,
+        "is_paid" : false,
+        "type" : "video",
+        "height" : 1080,
+        "playlist" : {
+          "fhd" : {
+            "format" : "",
+            "height" : ,
+            "channels" : ,
+            "play_url" : "",
+            "fps" : 25,
+            "duration" : ,
+            "size" : ,
+            "width" : ,
+            "bitrate" : ,
+            "sample_rate" : ,
+            "url" : ""
+          }
+        },
+        "duration" : ,
+        "thumbnail" : "",
+        "is_trial" : false,
+        "play_count" : ,
+        "is_open_bullet" : true
+      },
+      "topics" : [
+        {
+          "topic_type" : "",
+          "id" : "",
+          "url" : "",
+          "type" : "topic",
+          "name" : ""
+        }
+      ],
+      "excerpt" : "",
+      "liked_count" : ,
+      "updated_at" : ,
+      "collection" : {
+        "image_path" : "",
+        "zvideo_count" : ,
+        "author" : {
+          "url_token" : "",
+          "is_org" : false,
+          "url" : "",
+          "id" : "",
+          "is_following" : false,
+          "actived_at" : ,
+          "use_default_avatar" : false,
+          "avatar_url_template" : "",
+          "type" : "",
+          "name" : "",
+          "avatar_url" : ""
+        },
+        "id" : "",
+        "collection_id" : "",
+        "created_at" : ,
+        "voteup_count" : ,
+        "description" : "",
+        "name" : ""
+      },
+      "title" : "",
+      "admin_closed_comment" : false,
+      "is_reviewing" : false,
+      "interactive_tags" : [
+        {
+          "text" : ""
+        }
+      ],
+      "voteup_count" : 
+    }
   ];
 obj.paging = {
     "is_end" : true,
