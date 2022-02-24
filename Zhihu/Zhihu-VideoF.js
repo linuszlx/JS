f91c8373ff47ca42c31c25a86561fa6648bafdd6
linuszlx <linuszhang@126.com>
2022-02-24 13:58:31 +0800

    Create Zhihu-VideoF.js

---
+++ Zhihu/Zhihu-VideoF.js
@@ -0,0 +1,12 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data = [
+  ];
+obj.paging = {
+    "is_end" : false,
+    "previous" : "",
+    "totals" : 0,
+    "next" : "https:\/\/api.zhihu.com\/zvideo-tabs\/tabs\/choice\/feeds\/follow",
+    "is_start" : true
+  };
+$done({body: JSON.stringify(obj)});
