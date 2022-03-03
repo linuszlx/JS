1d5de778144c2b4ff5839606f3d65743a29850a7
linuszlx <linuszhang@126.com>
2022-03-03 17:19:47 +0800

    Update JD_PersonPage.js

--- JD/JD-Optimize/JD_PersonPage.js
+++ JD/JD-Optimize/JD_PersonPage.js
@@ -1,6 +1,4 @@
 let body = JSON.parse($response.body);
-
- delete body['floors'][2]['data']['logisticsInfo'];
   body.floors = body.floors.filter(function(item) {
     if (item.mId == "commonBanner" || item.mId == "myownerfloor_wodezhuanshu" || item.mId == "recommendfloor"|| item.mId == "horizontalCard") {
       return false;
