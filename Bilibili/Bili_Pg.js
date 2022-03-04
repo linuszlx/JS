d6f642a473d8f2a0a7be2fe87e6542366c542e9d
linuszlx <linuszhang@126.com>
2022-03-04 08:09:43 +0800

    Update Bili_Pg.js

--- Bilibili/Bili_Pg.js
+++ Bilibili/Bili_Pg.js
@@ -1,40 +1,41 @@
-let body = JSON.parse($response.body);
-// 删除了“推荐服务”
-body['data']['sections_v2'] = [
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data.sections_v2 = [
+      {
         "items" : [
           {
             "id" : 396,
             "title" : "离线缓存",
-            "icon" : "http://i0.hdslb.com/bfs/archive/5fc84565ab73e716d20cd2f65e0e1de9495d56f8.png",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/5fc84565ab73e716d20cd2f65e0e1de9495d56f8.png",
             "common_op_item" : {
 
             },
-            "uri" : "bilibili://user_center/download"
+            "uri" : "bilibili:\/\/user_center\/download"
           },
           {
             "id" : 397,
             "title" : "历史记录",
-            "icon" : "http://i0.hdslb.com/bfs/archive/8385323c6acde52e9cd52514ae13c8b9481c1a16.png",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/8385323c6acde52e9cd52514ae13c8b9481c1a16.png",
             "common_op_item" : {
 
             },
-            "uri" : "bilibili://user_center/history"
+            "uri" : "bilibili:\/\/user_center\/history"
           },
           {
             "id" : 398,
             "title" : "我的收藏",
-            "icon" : "http://i0.hdslb.com/bfs/archive/d79b19d983067a1b91614e830a7100c05204a821.png",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/d79b19d983067a1b91614e830a7100c05204a821.png",
             "common_op_item" : {
 
             },
-            "uri" : "bilibili://user_center/favourite"
+            "uri" : "bilibili:\/\/user_center\/favourite"
           },
           {
             "id" : 399,
             "title" : "稍后再看",
-            "icon" : "http://i0.hdslb.com/bfs/archive/63bb768caa02a68cb566a838f6f2415f0d1d02d6.png",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/63bb768caa02a68cb566a838f6f2415f0d1d02d6.png",
             "need_login" : 1,
-            "uri" : "bilibili://user_center/watch_later",
+            "uri" : "bilibili:\/\/user_center\/watch_later",
             "common_op_item" : {
 
             }
@@ -47,13 +48,13 @@ body['data']['sections_v2'] = [
       },
       {
         "button" : {
-          "icon" : "http://i0.hdslb.com/bfs/archive/205f47675eaaca7912111e0e9b1ac94cb985901f.png",
+          "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/205f47675eaaca7912111e0e9b1ac94cb985901f.png",
           "style" : 1,
-          "url" : "bilibili://uper/user_center/archive_selection",
+          "url" : "bilibili:\/\/uper\/user_center\/archive_selection",
           "text" : "有奖发布"
         },
         "style" : 1,
-        "tip_icon" : "http://i0.hdslb.com/bfs/feed-admin/8ea7064cfee183b523dc97971bdb86a0bd569aeb.png",
+        "tip_icon" : "http:\/\/i0.hdslb.com\/bfs\/feed-admin\/8ea7064cfee183b523dc97971bdb86a0bd569aeb.png",
         "title" : "创作中心",
         "type" : 1,
         "be_up_title" : "发布你的第一个视频",
@@ -63,39 +64,81 @@ body['data']['sections_v2'] = [
             "need_login" : 1,
             "display" : 1,
             "id" : 171,
-            "title" : "创作首页",
+            "title" : "创作中心",
             "global_red_dot" : 1,
-            "uri" : "bilibili://uper/homevc",
-            "icon" : "http://i0.hdslb.com/bfs/archive/d3aad2d07538d2d43805f1fa14a412d7a45cc861.png"
+            "uri" : "bilibili:\/\/uper\/homevc",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/d3aad2d07538d2d43805f1fa14a412d7a45cc861.png"
           },
           {
             "need_login" : 1,
             "display" : 1,
-            "id" : 172,
-            "title" : "稿件管理",
+            "id" : 570,
+            "title" : "UP主推荐",
             "global_red_dot" : 1,
-            "uri" : "bilibili://uper/user_center/archive_list",
-            "icon" : "http://i0.hdslb.com/bfs/archive/97acb2d8dec09b296a38f7f7093d651947d13b91.png"
+            "uri" : "https:\/\/member.bilibili.com\/york\/up-invitation\/latest?navhide=1",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/feed-admin\/621e25266b15b2bf311eeb4314e84310836bd4d6.png"
           },
           {
             "need_login" : 1,
             "display" : 1,
-            "id" : 534,
-            "title" : "打卡挑战",
+            "id" : 542,
+            "title" : "投稿领奖励",
             "global_red_dot" : 1,
-            "uri" : "https://member.bilibili.com/york/punch-card?navhide=1",
-            "icon" : "http://i0.hdslb.com/bfs/archive/68e553ee0675b45ebff98d2b0ec44501b558cc36.png"
+            "uri" : "https:\/\/member.bilibili.com\/studio\/bds\/redirect?target_source=mission_archive_2&channel=index",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/ae18624fd2a7bdda6d95ca606d5e4cf2647bfa4d.png"
           },
           {
             "need_login" : 1,
             "display" : 1,
             "id" : 174,
             "title" : "有奖活动",
+            "red_dot" : 1,
             "global_red_dot" : 1,
-            "uri" : "https://www.bilibili.com/blackboard/x/activity-tougao-h5/all",
-            "icon" : "http://i0.hdslb.com/bfs/archive/7f4fa86d99bf3814bf10f8ee5d6c8c9db6e931c8.png"
+            "uri" : "https:\/\/member.bilibili.com\/york\/hot-activity",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/7f4fa86d99bf3814bf10f8ee5d6c8c9db6e931c8.png"
           }
         ]
+      },
+      {
+        "title" : "推荐服务",
+        "items" : [
+          {
+            "id" : 400,
+            "title" : "我的课程",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/aa3a13c287e4d54a62b75917dd9970a3cde472e1.png",
+            "common_op_item" : {
+
+            },
+            "uri" : "https:\/\/m.bilibili.com\/cheese\/mine?navhide=1"
+          },
+          {
+            "id" : 402,
+            "title" : "个性装扮",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/0bcad10661b50f583969b5a188c12e5f0731628c.png",
+            "common_op_item" : {
+
+            },
+            "uri" : "https:\/\/www.bilibili.com\/h5\/mall\/home?navhide=1&from=myservice"
+          },
+          {
+            "id" : 404,
+            "title" : "我的钱包",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/f416634e361824e74a855332b6ff14e2e7c2e082.png",
+            "common_op_item" : {
+
+            },
+            "uri" : "bilibili:\/\/bilipay\/mine_wallet"
+          },
+          {
+            "id" : 544,
+            "title" : "创作中心",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/a879489af0406067c39940316396ae63aeefe088.png",
+            "need_login" : 1,
+            "uri" : "bilibili:\/\/uper\/homevc",
+            "common_op_item" : {
+
+            }
+          },
       },
       {
         "title" : "更多服务",
@@ -103,20 +146,20 @@ body['data']['sections_v2'] = [
           {
             "id" : 407,
             "title" : "联系客服",
-            "icon" : "http://i0.hdslb.com/bfs/archive/7ca840cf1d887a45ee1ef441ab57845bf26ef5fa.png",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/7ca840cf1d887a45ee1ef441ab57845bf26ef5fa.png",
             "common_op_item" : {
 
             },
-            "uri" : "bilibili://user_center/feedback"
+            "uri" : "bilibili:\/\/user_center\/feedback"
           },
           {
             "id" : 410,
             "title" : "设置",
-            "icon" : "http://i0.hdslb.com/bfs/archive/e932404f2ee62e075a772920019e9fbdb4b5656a.png",
+            "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/e932404f2ee62e075a772920019e9fbdb4b5656a.png",
             "common_op_item" : {
 
             },
-            "uri" : "bilibili://user_center/setting"
+            "uri" : "bilibili:\/\/user_center\/setting"
           }
         ],
         "style" : 2,
@@ -124,5 +167,5 @@ body['data']['sections_v2'] = [
 
         }
       }
-];
-$done({body: JSON.stringify(body)});
+    ];
+$done({body: JSON.stringify(obj)});
