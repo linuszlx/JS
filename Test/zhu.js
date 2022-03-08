5a706c7d3b11f02d3c2916b3e4cd95e2963865c3
linuszlx <linuszhang@126.com>
2022-03-08 16:47:10 +0800

    Update zhu.js

--- Test/zhu.js
+++ Test/zhu.js
@@ -11,4 +11,51 @@ body["config"]["mqtt_post_log"] = {
       "maxLogCount" : 0,
       "sampling" : 50
     };
+body["config"]["sentry_logReport"] = false;
+body["config"]["log_config"] = {
+      "monitors" : {
+        "initiative" : {
+          "enable" : false,
+          "probability" : 1
+        },
+        "hybrid" : {
+          "enable" : false,
+          "probability" : 1
+        },
+        "block" : {
+          "probability" : 1,
+          "enable" : false,
+          "calm_down_time" : 10,
+          "check_period_time" : 2,
+          "timeout" : 3
+        },
+        "initiativeFullLog" : {
+          "enable" : false,
+          "probability" : 1
+        },
+        "crash" : {
+          "enable" : false,
+          "probability" : 1
+        },
+        "metrics" : {
+          "time_interval" : 30,
+          "cpu" : {
+            "enable" : false,
+            "probability" : 1,
+            "threshold" : 6000
+          },
+          "memory" : {
+            "enable" : false,
+            "probability" : 1,
+            "threshold" : 10737418240
+          }
+        },
+        "remoteInstructionFullLog" : {
+          "enable" : false,
+          "probability" : 1
+        }
+      },
+      "description" : "针对日志回捞服务的AppCloud配置"
+    };
+
 $done({body: JSON.stringify(body)});
