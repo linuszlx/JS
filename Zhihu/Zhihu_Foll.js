1c1557dc737fcaefb3b518732e08442657c34f49
linuszlx <linuszhang@126.com>
2022-03-31 22:08:47 +0800

    Update Zhihu_Foll.js

--- Zhihu/Zhihu_Foll.js
+++ Zhihu/Zhihu_Foll.js
@@ -1,14 +1,8 @@
-const path1 = "moments_v2\?";
-
-let url = $request.url;
 let body = JSON.parse($response.body);
-
-if (url.indexOf(path1) != -1) {
   body.data = body.data.filter(function(item) {
-    if (item.type == "zvideo") {
+    if (item.target_type == "zvideo") {
       return false;
     }
     return true;
   });                                       
-}
 $done({body: JSON.stringify(body)});
