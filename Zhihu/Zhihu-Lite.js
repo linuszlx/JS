4778caf2cfdde63482a5f04cc047167a3d6d8259
linuszlx <linuszhang@126.com>
2022-03-31 22:46:27 +0800

    Update Zhihu-Lite.js

--- Zhihu/Zhihu-Lite.js
+++ Zhihu/Zhihu-Lite.js
@@ -48,8 +48,8 @@ switch (true) {
             for (let i = 0; i < obj['data'].length; i++) {
                 let element = targetIdFix(obj['data'][i]);
                 if (!element['ad']) 
-				        // 屏蔽关注页的“最新视频”
-        else if (element["type"] = "videos"){
+               // 屏蔽关注页的“最新视频”
+            else if (element["type"] = "videos"){
                     data.push(element);
                 }
             }
