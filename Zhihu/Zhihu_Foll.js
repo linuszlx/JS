967c596cc07a1f9e64f725eed97d30c7f405065f
linuszlx <linuszhang@126.com>
2022-04-01 11:32:35 +0800

    Delete Zhihu_Foll.js

--- Zhihu/Zhihu_Foll.js
+++
@@ -1,8 +0,0 @@
-let body = JSON.parse($response.body);
-  body.data = body.data.filter(function(item) {
-    if (item.type == "videos") {
-      return false;
-    }
-    return true;
-  });                                       
-$done({body: JSON.stringify(body)});
