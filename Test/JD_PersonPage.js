ad7d2f8426a49ffde3f01bd01398f270fc0fd65e
linuszlx <linuszhang@126.com>
2022-04-02 16:51:04 +0800

    Delete JD_PersonPage.js

--- Test/JD_PersonPage.js
+++
@@ -1,9 +0,0 @@
-let body = JSON.parse($response.body);
-  body.floors = body.floors.filter(function(item) {
-    if (item.mId == "commonBanner" || item.mId == "myownerfloor_wodezhuanshu" || item.mId == "recommendfloor"|| item.mId == "horizontalCard") {
-      return false;
-    }
-    return true;
-  });
-  body['floors']['2']['data']['logisticsInfo'] = { };
-$done({body: JSON.stringify(body)});
