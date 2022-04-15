7a2b77c2604c94e0d24c89668de63bbf72852a37
linuszlx <linuszhang@126.com>
2022-04-16 01:06:01 +0800

    Update Bili_Auto_Regions.js

--- Bilibili/Bili_Auto_Regions.js
+++ Bilibili/Bili_Auto_Regions.js
@@ -80,7 +80,7 @@ async function SwitchRegion(play) {
 	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
 	const TW = $.read('BiliArea_TW') || '台湾节点'; //Your Taiwan sub-policy name.
 	const HK = $.read('BiliArea_HK') || '香港节点'; //Your HongKong sub-policy name.
-	const DF = $.read('BiliArea_DF') || ''; //Sub-policy name used after region is blocked(e.g. url 404)
+	const DF = $.read('BiliArea_DF') || '台湾节点'; //Sub-policy name used after region is blocked(e.g. url 404)
 	const off = $.read('BiliArea_disabled') || ''; //WiFi blacklist(disable region change), separated by commas.
 	const current = await $.getPolicy(Group);
 	const area = (() => {
