870c47af86de981d3df04bf835ca4e552af57e32
linuszlx <linuszhang@126.com>
2022-04-16 22:28:06 +0800

    Update Bili_pgc_home.js

--- Bilibili/Bili_pgc_home.js
+++ Bilibili/Bili_pgc_home.js
@@ -1,6 +1,6 @@
 let body = JSON.parse($response.body);
   body.result.modules = body.result.modules.filter(function(item) {
-    if (item.module_id == 241 || item.module_id == 1545 || item.module_id == 1283) {
+    if (item.module_id == 1545 || item.module_id == 1283) {
       return false;
     }
     return true;
