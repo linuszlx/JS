74a4e7cc8ebae8e9453170ea8b3d75838ee3da1a
linuszlx <linuszhang@126.com>
2022-04-17 11:22:35 +0800

    Update BiliDns.js

--- Bilibili/BiliDns.js
+++ Bilibili/BiliDns.js
@@ -1,8 +1,4 @@
-let body = JSON.parse($response.body);
-  body.dns = body.dns.filter(function(item) {
-    if (item.type == 1) {
-      return false;
-    }
-    return true;
-  });
-$done({body: JSON.stringify(body)});
+var body = $response.body;
+var obj = JSON.parse(body);
+obj['dns']['0']['ips'] = [ ];
+$done({body: JSON.stringify(obj)});
