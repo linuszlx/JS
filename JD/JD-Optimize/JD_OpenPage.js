4ba5d49407dfe6afdea23e47c785eeb7336cb19c
linuszlx <linuszhang@126.com>
2022-05-23 17:44:24 +0800

    Update JD_OpenPage.js

--- JD/JD-Optimize/JD_OpenPage.js
+++ JD/JD-Optimize/JD_OpenPage.js
@@ -8,9 +8,9 @@ if (url.indexOf(path1) != -1) {
   body.futureFloorList = []; 
   body.floorList = body.floorList.filter(function(item) {
     if (item.floorId == 9493 || item.floorId == 9915 || item.floorId == 7531 || item.floorId == 7096 || item.floorId == 9516 || item.floorId == 8407) {
-      return true;
+      return false;
     }
-    return false;
+    return true;
   });                                       
 }
 $done({body: JSON.stringify(body)});
