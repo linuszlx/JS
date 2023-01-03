6065d03ade195a592450a1e3571a6ab77cc0c511
linuszlx <linuszhang@126.com>
2023-01-03 15:08:45 +0800

    Update JD_OpenPage.js

--- JD/JD-Optimize/JD_OpenPage.js
+++ JD/JD-Optimize/JD_OpenPage.js
@@ -3,11 +3,11 @@ const path1 = "welcomeHome";
 let url = $request.url;
 let body = JSON.parse($response.body);
 if (url.indexOf(path1) != -1) {
-  body.topBgImgBig = "https:\/\/m15.360buyimg.com\/mobilecms\/s1125x939_jfs\/t1\/183265\/6\/24069\/210481\/626b97cdEa9b711f6\/f8feeeaafa671104.png!q70.jpg";
+  body.topBgImgBig = ;
   body.webViewFloorList = [];
   body.futureFloorList = []; 
   body.floorList = body.floorList.filter(function(item) {
-    if (item.floorId == 9924 || item.floorId == 9421 || item.floorId == 10284 || item.floorId == 8407){
+    if (item.floorId == 9924 || item.floorId == 9421 || item.floorId == 10309 || item.floorId == 8407){
       return true;
     }
     return false;
