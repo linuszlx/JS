b41ae31248f1dd3ed4105e49e6c2683c3bfc6a93
linuszlx <linuszhang@126.com>
2023-01-03 15:45:17 +0800

    Update JD_OpenPage.js

--- JD/JD-Optimize/JD_OpenPage.js
+++ JD/JD-Optimize/JD_OpenPage.js
@@ -3,11 +3,11 @@ const path1 = "welcomeHome";
 let url = $request.url;
 let body = JSON.parse($response.body);
 if (url.indexOf(path1) != -1) {
-  body.topBgImgBig = "https:\/\/m.360buyimg.com\/mobilecms\/jfs\/t16789\/324\/2566217976\/2179\/58b27e89\/5af962e6N61fbd832.png";
+  body.topBgImgBig = "https:\/\/m15.360buyimg.com\/mobilecms\/s1125x939_jfs\/t1\/129285\/30\/21934\/123475\/62013269E02faf891\/ee9f510a656de1dc.png!q70.jpg";
   body.webViewFloorList = [];
   body.futureFloorList = []; 
   body.floorList = body.floorList.filter(function(item) {
-    if (item.floorOrder == -2 || item.floorOrder == 11 || item.floorOrder == 23 || item.floorOrder == 494){
+    if (item.floorOrder == -2 || item.floorOrder == 11 || item.floorOrder == 23 || item.floorId == 8407){
       return true;
     }
     return false;
