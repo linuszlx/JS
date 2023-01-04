dece9686e44f3237c1c66de4e30edec73146d4f0
linuszlx <linuszhang@126.com>
2023-01-04 10:28:52 +0800

    Update JD_PersonPage.js

--- JD/JD-Optimize/JD_PersonPage.js
+++ JD/JD-Optimize/JD_PersonPage.js
@@ -1,10 +1,10 @@
 let body = JSON.parse($response.body);
   body.floors = body.floors.filter(function(item) {
-    if (item.mId == "commonBanner" || item.mId == "myownerfloor_wodezhuanshu" || item.mId == "recommendfloor"|| item.mId == "horizontalCard") {
+    if (item.mId == "commonBanner" || item.mId == "recommendfloor"|| item.mId == "horizontalCard" || item.mId == "buyOften" || item.mId == "platCard") {
       return false;
     }
     return true;
   });
-if(body.floors.hasOwnProperty('1')){body['floors']['1']['data']['logisticsInfo'] = { }};
-if(body.floors.hasOwnProperty('1')){body['floors']['1']['data']['commentRemindInfo'] = { }};
+if(body.floors.hasOwnProperty('1')){body['floors']['2']['data']['logisticsInfo'] = { }};
+if(body.floors.hasOwnProperty('1')){body['floors']['2']['data']['commentRemindInfo'] = { }};
 $done({body: JSON.stringify(body)});
