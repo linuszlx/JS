045b813fa867d329776f4c192a8a5cbc6b0fb78c
linuszlx <linuszhang@126.com>
2023-01-17 22:04:56 +0800

    Update FlyTea_MD.js

--- FlyTea/FlyTea_MD.js
+++ FlyTea/FlyTea_MD.js
@@ -1,4 +1,16 @@
 var body = $response.body;
 var obj = JSON.parse(body);
+obj.Variables.extgroupidsdata = [
+      {
+        "groupid" : "52",
+        "groupicon" : null,
+        "grouptitle" : null
+      },
+      {
+        "groupid" : "30",
+        "groupicon" : null,
+        "grouptitle" : null
+      }
+    ];
 obj.Variables.extgroupvip = "4";
 $done({body: JSON.stringify(obj)});
