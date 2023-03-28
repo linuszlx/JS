b4dabac4dbfc7c84ba9093a6cd4dca25d2d4b54f
linuszlx <linuszhang@126.com>
2023-03-29 05:02:31 +0800

    Update 12306.js

--- 12306/12306.js
+++ 12306/12306.js
@@ -35,4 +35,4 @@ hostname = ad.12306.cn
 
 const version = 'V1.0.22';
 
-var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"https://www.yuanxiapi.cn/api/bing/",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.showSkipBtn=1,ddgksf2013.code="00"):ddgksf2013="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
+var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"https://www.yuanxiapi.cn/api/bing/",creativeType:1}],ddgksf2013.advertParam={skipTime:1,showSkipBtn=1,},ddgksf2013.code="00"):ddgksf2013="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
