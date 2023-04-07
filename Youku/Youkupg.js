9d88a9a36bbd35c1229073e8088180d4960a1966
linuszlx <linuszhang@126.com>
2023-04-07 09:06:46 +0800

    Create Youkupg.js

---
+++ Youku/Youkupg.js
@@ -0,0 +1,124 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data.model.configInfo.bottomNavigate.data.bottomTabList = [
+              {
+                "androidClickRouteFullClassName" : "com.youku.v2.HomePageEntry",
+                "arg1" : "home_new_1",
+                "unClickTextTone" : "#ff777777",
+                "lottieSourceUrl" : "http://ykimg.alicdn.com/develop/image/2022-12-12/8a4d2036b6ac73f1bc79d7358bfcb3df.json",
+                "clickColor" : "ykn_primaryInfo",
+                "lottieSourceDarkUrl" : "http://ykimg.alicdn.com/develop/image/2022-12-12/72bd3b572c779a379bf487b221742ffe.json",
+                "skinIdentityAssociated" : "HOME",
+                "specialInfo" : {
+                  "lottieA2B" : "http://ykimg.alicdn.com/develop/image/2022-12-06/ecc15e443b55401a38442e1fb3d0c371.json",
+                  "status" : 1,
+                  "lottieBDark" : "http://ykimg.alicdn.com/develop/image/2022-12-12/c8b051c1cccc0064f999f6a271843da6.json",
+                  "clickedIconADark" : "http://liangcang-material.alicdn.com/prod/upload/24ee4448b89c431db364405f406fc375.webp.png",
+                  "clickedIconBDark" : "http://liangcang-material.alicdn.com/prod/upload/136055627ad543268325b9855226acb1.webp.png",
+                  "unClickedIconDark" : "http://liangcang-material.alicdn.com/prod/upload/ebf9805ac50444bb97447eb4e56a0a1a.webp.png",
+                  "lottieB" : "http://ykimg.alicdn.com/develop/image/2022-12-12/b4d8fdbf26a2475d9d897c8aec2ac530.json",
+                  "lottieB2A" : "http://ykimg.alicdn.com/develop/image/2022-12-06/8f306fb14498d4e6a81bf5eff1d29ffe.json",
+                  "unClickedIcon" : "http://liangcang-material.alicdn.com/prod/upload/6fea112af73541be83266fa96aa19e0a.webp.png",
+                  "clickedColor" : "#ff000000",
+                  "clickedIconA" : "http://liangcang-material.alicdn.com/prod/upload/716b1c8a2d9043f4a6ce5b15d7a59ab2.webp.png",
+                  "unClickedColor" : "#ff777777",
+                  "lottieA2BDark" : "http://ykimg.alicdn.com/develop/image/2022-12-06/f5520755167f9a3a8e6601404e1678aa.json",
+                  "clickedIconB" : "http://liangcang-material.alicdn.com/prod/upload/eb704894e0a0441e9dcab220ae705b03.webp.png",
+                  "lottieA" : "http://ykimg.alicdn.com/develop/image/2022-12-12/c08570cf6193b0f7ab88e7803d42b9da.json",
+                  "lottieB2ADark" : "http://ykimg.alicdn.com/develop/image/2022-12-06/71a22c0b9b3030e965257b9305bdfee4.json",
+                  "lottieADark" : "http://ykimg.alicdn.com/develop/image/2022-12-12/5aa3acb7a47acd98963c0a9bc57161e2.json"
+                },
+                "typeName" : "首页",
+                "iconStyle" : "small",
+                "extInfo" : {
+                  "specialInfoNew" : {
+                    "status" : 0
+                  }
+                },
+                "clickIconDark" : "http://liangcang-material.alicdn.com/prod/upload/e0de82a559e34c02a72318f42febe192.webp.png",
+                "spm" : "a2h0f.8166709.home_new.1",
+                "clickTextTone" : "#ff222222",
+                "type" : "HOME",
+                "unClickIcon" : "http://liangcang-material.alicdn.com/prod/upload/0370bce014db4c9d8a4a4888fbbf6191.webp.png",
+                "id" : 229,
+                "unClickColor" : "ykn_secondaryInfo",
+                "menuIndex" : 1,
+                "clickRouteClassName" : "HomeViewController",
+                "clickIcon" : "http://liangcang-material.alicdn.com/prod/upload/4027f89eff9a40e498735d0a1415da1c.webp.png",
+                "unClickToken" : "ykn_secondaryInfo",
+                "unClickIconDark" : "http://liangcang-material.alicdn.com/prod/upload/d834b0f916a7415c970c452c566a203d.webp.png"
+              },
+              {
+                "androidClickRouteFullClassName" : "com.youku.vip.wrapper.VipHomeActivity",
+                "arg1" : "vipswift_1",
+                "pendant" : {
+                  "number" : 1,
+                  "ceilingTimes" : 5,
+                  "pendantType" : "marketPoint",
+                  "validity" : 1,
+                  "interviewTime" : 86400000
+                },
+                "lottieSourceUrl" : "http://ykimg.alicdn.com/develop/image/2022-12-15/1cdd72cea1c0be45bc61062d01cde0b2.json",
+                "clickColor" : "#D0992E",
+                "lottieSourceDarkUrl" : "http://ykimg.alicdn.com/develop/image/2022-12-15/11d324eb0ccb59d5c942b1da00127a25.json",
+                "skinIdentityAssociated" : "VIP_MEMBER",
+                "specialInfo" : {
+                  "status" : 0
+                },
+                "typeName" : "会员",
+                "iconStyle" : "small",
+                "unClickTextTone" : "#ff777777",
+                "extInfo" : {
+                  "specialInfoNew" : {
+                    "status" : 0
+                  }
+                },
+                "clickIconDark" : "http://liangcang-material.alicdn.com/prod/upload/fc490900ad144f5eb4f7d6673743a111.webp.png",
+                "spm" : "a2h0f.8166709.vipswift.1",
+                "clickTextTone" : "#ffd0992e",
+                "schemaUrl" : "",
+                "type" : "VIP_MEMBER",
+                "unClickIcon" : "http://liangcang-material.alicdn.com/prod/upload/9728e30d78b94af8921b3a7717104e12.webp.png",
+                "id" : 227,
+                "unClickColor" : "ykn_secondaryInfo",
+                "menuIndex" : 3,
+                "clickRouteClassName" : "VIP_Main_ViewController_Swift",
+                "clickIcon" : "http://liangcang-material.alicdn.com/prod/upload/72c62822ef48451fbecd9fd70cd71afd.webp.png",
+                "unClickToken" : "ykn_secondaryInfo",
+                "unClickIconDark" : "http://liangcang-material.alicdn.com/prod/upload/e2abb2b6ef4e401e8d5f911f0d2ce90c.webp.png"
+              },
+              {
+                "androidClickRouteFullClassName" : "com.youku.usercenter.activity.UserCenterActivity",
+                "arg1" : "spaceswift_1",
+                "unClickTextTone" : "#ff777777",
+                "lottieSourceUrl" : "http://ykimg.alicdn.com/develop/image/2022-12-15/0a13eeffb6229a37f677c55f7c8c0247.json",
+                "clickColor" : "ykn_primaryInfo",
+                "lottieSourceDarkUrl" : "http://ykimg.alicdn.com/develop/image/2022-12-15/41f7c90546a00067e08066d666f69780.json",
+                "skinIdentityAssociated" : "NEW_UCENTER",
+                "specialInfo" : {
+                  "status" : 0
+                },
+                "typeName" : "我的",
+                "iconStyle" : "small",
+                "extInfo" : {
+                  "specialInfoNew" : {
+                    "status" : 1,
+                    "deliveryRuleIds" : "",
+                    "scene" : "NFT_HEAD"
+                  }
+                },
+                "clickIconDark" : "http://liangcang-material.alicdn.com/prod/upload/eaafd4cfd43a44bc9831d62003e524b6.webp.png",
+                "spm" : "a2h0f.8166709.spaceswift.1",
+                "clickTextTone" : "#ff222222",
+                "type" : "NEW_UCENTER",
+                "unClickIcon" : "http://liangcang-material.alicdn.com/prod/upload/fa1b43fea8a14aba89fc6c9cf0236a52.webp.png",
+                "id" : 225,
+                "unClickColor" : "ykn_secondaryInfo",
+                "menuIndex" : 5,
+                "clickRouteClassName" : "USMainViewController",
+                "clickIcon" : "http://liangcang-material.alicdn.com/prod/upload/ebfdb8d97fa4445dae12834fef5cb8b9.webp.png",
+                "unClickToken" : "ykn_secondaryInfo",
+                "unClickIconDark" : "http://liangcang-material.alicdn.com/prod/upload/3f1e8173ccca493db46d5a34a22c4cc5.webp.png"
+              }
+            ];
+$done({body: JSON.stringify(obj)});
