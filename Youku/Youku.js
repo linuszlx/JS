f275369fa5035c39a6e0ec44e3dd8b1c8bf0da76
linuszlx <linuszhang@126.com>
2023-04-07 17:21:22 +0800

    Update Youku.js

--- Youku/Youku.js
+++ Youku/Youku.js
@@ -1,5 +1,5 @@
 let body = JSON.parse($response.body);
-if(body.data.hasOwnProperty('2019061000')){body.data.2019061000.data.nodes = body.data.2019061000.data.nodes.filter(function(item) {
+if(body.data.2019061000.data.nodes.hasOwnProperty('2')){body.data.2019061000.data.nodes = body.data.2019061000.data.nodes.filter(function(item) {
     if (item.data.title == "精选" || item.data.title == "电视剧" || item.data.title == "电影" || item.data.title == "综艺" || item.data.title == "动漫" || item.data.title == "少儿" || item.data.title == "纪录片" ||){
       return true;
     }
