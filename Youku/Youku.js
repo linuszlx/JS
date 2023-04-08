d3519594dcea0a2ce697c85c9ec42bd3035736e9
linuszlx <linuszhang@126.com>
2023-04-08 14:46:13 +0800

    Update Youku.js

--- Youku/Youku.js
+++ Youku/Youku.js
@@ -1,4 +1,4 @@
 let body = JSON.parse($response.body);
-if(body['data']['2019061000']['data']['nodes'].hasOwnProperty("3")){body['data']['2019061000']['data']['nodes']['3']['nodes']['0'] = { }};
-if(body['data']['2019061000']['data']['nodes'].hasOwnProperty("4")){body['data']['2019061000']['data']['nodes']['4']['nodes']['0'] = { }};
+if(body['data']['2019061000']['data']['nodes'].hasOwnProperty("3")){body['data']['2019061000']['data']['nodes']['1']['nodes']['0'] = { }};
+if(body['data']['2019061000']['data']['nodes'].hasOwnProperty("4")){body['data']['2019061000']['data']['nodes']['1']['nodes']['0'] = { }};
 $done({body: JSON.stringify(body)});
