18c6bbff80ca7e4feb4ebad1e9750f9704ae5620
linuszlx <linuszhang@126.com>
2023-04-08 09:55:21 +0800

    Update YoukuP.js

--- Youku/YoukuP.js
+++ Youku/YoukuP.js
@@ -1,8 +1,8 @@
 let body = JSON.parse($response.body);
-  body.data.nodes = body.data.nodes.filter(function(item) {
+  body['data']['2019061000']['data']['nodes'][0]['nodes']= body['data']['2019061000']['data']['nodes'][0]['nodes'].filter(function(item) {
     if (item.id == 28912 || item.id == 110429 || item.id == 36021 || item.id == 35640|| item.id == 36015){
       return true;
-    }
+          }
     return false;
   });
 $done({body: JSON.stringify(body)});
