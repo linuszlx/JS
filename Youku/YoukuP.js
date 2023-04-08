3a6ff3631bfde2cb174adeb5034bdfa1f7edea89
linuszlx <linuszhang@126.com>
2023-04-08 09:49:01 +0800

    Update YoukuP.js

--- Youku/YoukuP.js
+++ Youku/YoukuP.js
@@ -1,5 +1,5 @@
 let body = JSON.parse($response.body);
-  body.data.2019061000.data.nodes.0.nodes = body.data.2019061000.data.nodes.0.nodes.filter(function(item) {
+  body.data.nodes = body.data.nodes.filter(function(item) {
     if (item.id == 28912 || item.id == 110429 || item.id == 36021 || item.id == 35640|| item.id == 36015){
       return true;
     }
