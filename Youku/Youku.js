2f9dee7bd8ef0cfd85704fcea59100e06c9a0909
linuszlx <linuszhang@126.com>
2023-04-08 10:44:54 +0800

    Update Youku.js

--- Youku/Youku.js
+++ Youku/Youku.js
@@ -1,6 +1,4 @@
 let body = JSON.parse($response.body);
-let item3 = body['data']['2019061000']['data']['nodes'][3]['typeName']
-let item4 = body['data']['2019061000']['data']['nodes'][4]['typeName']
-if(item3.hasOwnProperty("FEED_DRAWER_PAGINATION")){body['data']['2019061000']['data']['nodes']['3']['nodes']['0'] = { }};
-if(item4.hasOwnProperty("FEED_DRAWER_PAGINATION")){body['data']['2019061000']['data']['nodes']['4']['nodes']['0'] = { }};
+if(body['data']['2019061000']['data']['nodes'][3]['typeName'].hasOwnProperty("FEED_DRAWER_PAGINATION")){body['data']['2019061000']['data']['nodes']['3']['nodes']['0'] = { }};
+if(body['data']['2019061000']['data']['nodes'][4]['typeName'].hasOwnProperty("FEED_DRAWER_PAGINATION")){body['data']['2019061000']['data']['nodes']['4']['nodes']['0'] = { }};
 $done({body: JSON.stringify(body)});
