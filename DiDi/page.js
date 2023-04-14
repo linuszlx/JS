20bef2c1e8e512ed5985f96a6bcab683ccba169d
linuszlx <linuszhang@126.com>
2023-04-14 13:53:06 +0800

    Update page.js

--- DiDi/page.js
+++ DiDi/page.js
@@ -5,28 +5,30 @@ let body = JSON.parse($response.body);
     }
     return false;
   });
-  body.data.disorder_cards.bottom_nav_list = [
-      {
-        "icon_active" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_r1NRbbajAxYAGmF8gdxD",
-        "id" : "v6x_home",
-        "text_active_color" : "#EA5E1E",
-        "text" : "滴滴",
-        "show_type" : "default",
-        "link" : "OneTravel:\/\/v6x_home\/entrance",
-        "icon_repeat_time" : 0,
-        "icon" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_tqiBMj2YSr6T1Z8iPPUa",
-        "icon_top" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_pP1QXmCdLbMMZ5dA3vcg"
-      },
-      {
-        "icon_active" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_q2UwLrTjnIiLPLWjqw9P",
-        "id" : "user_center",
-        "text_active_color" : "#EA5E1E",
-        "text" : "我的",
-        "show_type" : "default",
-        "link" : "OneTravel:\/\/user_center\/entrance",
-        "icon_repeat_time" : 0,
-        "icon" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_nJzY3uniNt8xzmESKM9X"
-      }
-    ]; 
+  body.data.disorder_cards.bottom_nav_list.data =[
+          {
+            "link" : "OneTravel:\/\/v6x_home\/entrance",
+            "icon_active" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_X6SIN2jy1VlvqRfKhn4l",
+            "id" : "v6x_home",
+            "text_active_color" : "#EA5E1E",
+            "text" : "首页",
+            "show_type" : "default",
+            "text_active" : "",
+            "icon_top" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_JX6d2A8ZAW7A4TahE72d",
+            "icon" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_vl77BvBGTsVM4jnymaXE",
+            "text_color" : "#757575"
+          },
+          {
+            "icon_active" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_9tKZulmflHQK8KAiyhlf",
+            "id" : "user_center",
+            "text_active_color" : "#EA5E1E",
+            "text" : "我的",
+            "show_type" : "default",
+            "text_active" : "我的",
+            "link" : "OneTravel:\/\/user_center\/entrance",
+            "icon" : "https:\/\/img-hxy021.didistatic.com\/static\/apollofile\/do1_9YT4vT5MLZnpc8Y9utfJ",
+            "text_color" : "#757575"
+          }
+        ]; 
 $done({body: JSON.stringify(body)});
 
