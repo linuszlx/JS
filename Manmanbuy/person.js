f986c7dd2268113a79805b8c4203b4d2e51c12e0
linuszlx <linuszhang@126.com>
2023-06-13 21:18:26 +0800

    Update person.js

--- Manmanbuy/person.js
+++ Manmanbuy/person.js
@@ -1,6 +1,6 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-if (obj.hasOwnProperty("data") == true){
+if (obj.data.hasOwnProperty("friendChannel") == true){
  obj['data']['friendChannel']= { };
  obj['data']['searchBeginScene']= { };
  obj['data']['hongbaoconfig_3642']= { };
