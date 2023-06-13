0de63704ec3bf54902c930b2c9bdfb8c2b8e8fe4
linuszlx <linuszhang@126.com>
2023-06-13 19:15:25 +0800

    Update person.js

--- Manmanbuy/person.js
+++ Manmanbuy/person.js
@@ -1,8 +1,10 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.data.friendChannel = { };
-obj.data.searchBeginScene = { };
-obj.data.hongbaoconfig_3642 = { };
-obj.data.commonChannel = { };
-obj.data.bcjChannel = { };
+if(body.data.hasOwnProperty('11'))
+{body['data']['friendChannel']= { };
+ body['data']['searchBeginScene']= { };
+ body['data']['hongbaoconfig_3642']= { };
+ body['data']['commonChannel']= { };
+ body['data']['bcjChannel']= { };
+};
 $done({body: JSON.stringify(obj)});
