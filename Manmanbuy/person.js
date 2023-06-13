9b0e7eee34c7e9ba8fbf1b4affa1e0c7dcfa31a4
linuszlx <linuszhang@126.com>
2023-06-13 19:46:43 +0800

    Update person.js

--- Manmanbuy/person.js
+++ Manmanbuy/person.js
@@ -1,3 +1,8 @@
-var body = $response.body
-    .replace(/adConfig/g, /abc/)
-$done({ body });
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data.friendChannel = { };
+obj.data.searchBeginScene = { };
+obj.data.hongbaoconfig_3642 = { };
+obj.data.commonChannel = { };
+obj.data.bcjChannel = { };
+$done({body: JSON.stringify(obj)});
