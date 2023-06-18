889ee0cc2bf593c8d06d1541c2ade35570fae76a
linuszlx <linuszhang@126.com>
2023-06-18 17:23:50 +0800

    Update smzdm-sq.js

--- Smzdm/smzdm-sq.js
+++ Smzdm/smzdm-sq.js
@@ -1,3 +1,3 @@
 let body = JSON.parse($response.body);
-body['data']['rows']['0'] = [];
+ if (body.data.hasOwnProperty("rows") == true) {body['data']['rows']['0'] = [];};
 $done({body: JSON.stringify(body)});
