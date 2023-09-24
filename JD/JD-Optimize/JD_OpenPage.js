c5b1031953a2f8ce8f3604a44ab09fd3b6489b64
linuszlx <linuszhang@126.com>
2023-09-25 01:06:40 +0800

    Update JD_OpenPage.js

--- JD/JD-Optimize/JD_OpenPage.js
+++ JD/JD-Optimize/JD_OpenPage.js
@@ -3,11 +3,11 @@ const path1 = "welcomeHome";
 let url = $request.url;
 let body = JSON.parse($response.body);
 if (url.indexOf(path1) != -1) {
-  body.topBgImgBig = "https:\/\/m15.360buyimg.com\/mobilecms\/s1125x939_jfs\/t1\/129285\/30\/21934\/123475\/62013269E02faf891\/ee9f510a656de1dc.png!q70.jpg";
+  body.topBgImgBig = "https:\/\/m15.360buyimg.com\/mobilecms\/s1125x939_jfs\/t1\/118114\/21\/34437\/69326\/64491c10F39c4e579\/5e4591bc1f8678e6.png";
   body.webViewFloorList = [];
   body.futureFloorList = []; 
   body.floorList = body.floorList.filter(function(item) {
-    if (item.floorOrder == -2 || item.floorOrder == 11 || item.floorOrder == 23 || item.floorId == 8407){
+    if (item.floorOrder == -2 || item.floorOrder == 11 || item.floorOrder == 23 || item.floorId == 470 || item.floorId == 472  || item.floorId == 474 || item.floorId == 482 || item.floorId == 484 || item.floorId == 488){
       return true;
     }
     return false;
