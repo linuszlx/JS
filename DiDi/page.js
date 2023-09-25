aa20b0656b40e7fc1a8880f7f6fc122d6dfe5d0a
linuszlx <linuszhang@126.com>
2023-09-26 00:04:56 +0800

    Update page.js

--- DiDi/page.js
+++ DiDi/page.js
@@ -1,6 +1,6 @@
 let body = JSON.parse($response.body);
   body.data.order_cards.nav_list_card.data = body.data.order_cards.nav_list_card.data.filter(function(item) {
-    if (item.link_title == "打车" || item.link_title == "代驾" || item.link_title == "滴滴青桔骑行" || item.link_title == "专车" || item.link_title == "更多" || item.link_title == "顺风车" || item.link_title == "滴滴租车" || item.link_title == "特价拼车" || item.link_title == "公交地铁" || item.link_title == "远途拼车"){
+    if (item.category_name == "出行用滴滴"){
       return true;
     }
     return false;
