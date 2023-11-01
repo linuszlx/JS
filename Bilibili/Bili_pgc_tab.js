49c7043d691dca5d87d25dd2cf067a68d49851a2
linuszlx <linuszhang@126.com>
2023-11-01 18:08:58 +0800

    Update Bili_pgc_tab.js

--- Bilibili/Bili_pgc_tab.js
+++ Bilibili/Bili_pgc_tab.js
@@ -1,8 +1,8 @@
 let body = JSON.parse($response.body);
-  body.result.modules = body.result.modules.filter(function(item) {
+  if(body.result.modules.hasOwnProperty('3')){body.result.modules = body.result.modules.filter(function(item) {
     if (item.module_id == 1058) {
       return false;
     }
     return true;
-  });
+  })};
 $done({body: JSON.stringify(body)});
