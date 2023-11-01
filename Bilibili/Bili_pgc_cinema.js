933abb370be9fb521709ac340d0da042329e696a
linuszlx <linuszhang@126.com>
2023-11-01 20:31:19 +0800

    Update Bili_pgc_cinema.js

--- Bilibili/Bili_pgc_cinema.js
+++ Bilibili/Bili_pgc_cinema.js
@@ -1,8 +1,8 @@
 let body = JSON.parse($response.body);
-  body.result.modules = body.result.modules.filter(function(item) {
-    if (item.module_id == 1441 || item.module_id == 1284) {
+  if(body.result.modules.hasOwnProperty('3')){body.result.modules = body.result.modules.filter(function(item) {
+    if (item.module_id == 219) {
       return false;
     }
     return true;
-  });
+  })};
 $done({body: JSON.stringify(body)});
