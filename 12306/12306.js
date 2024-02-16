5e7dae416cb7fea5623427d3092b15e97d9388c2
linuszlx <linuszhang@126.com>
2024-02-16 23:12:20 +0800

    Update 12306.js

--- 12306/12306.js
+++ 12306/12306.js
@@ -1,1 +1,15 @@
-!function(){let e,a=JSON.parse($request.body);e="0007"===a.placementNo?'{"materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}':"G0054"===a.placementNo?'{"code":"00","materialsList":[{}]}':'{"code":"00","message":"无广告返回"}';"undefined"!=typeof $task?$done({body:e}):$done({response:{body:e}})}();
+// 2023-02-22 18:20
+
+let body;
+let obj = JSON.parse($request.body);
+
+if (obj.placementNo === "0007") {
+  body =
+    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+} else if (obj.placementNo === "G0054") {
+  body = '{"code":"00","materialsList":[]}';
+} else {
+  body = '{"code":"00","message":"无广告返回"}';
+}
+
+$done({ body });
