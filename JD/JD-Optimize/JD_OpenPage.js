7d254799191f7d81b013d414a02ef7bf65deebf9
linuszlx <linuszhang@126.com>
2024-04-03 20:35:38 +0800

    Update JD_OpenPage.js

--- JD/JD-Optimize/JD_OpenPage.js
+++ JD/JD-Optimize/JD_OpenPage.js
@@ -3,7 +3,7 @@ let body = JSON.parse($response.body);
   body.webViewFloorList = [];
   body.futureFloorList = []; 
   body.floorList = body.floorList.filter(function(item) {
-    if (item.floorOrder == -2 ||  item.floorOrder == 11 || item.floorOrder == 23 || item.floorOrder == 106 || item.floorOrder == 113 || item.floorOrder == 120 || item.floorOrder == 126 || item.floorOrder == 470 || item.floorOrder == 472 || item.floorOrder == 474 || item.floorOrder == 493|| item.floorId == 8407|| item.floorId == 497){
+    if (item.floorOrder == -2 ||  item.floorOrder == 11 || item.floorOrder == 23 || item.floorOrder == 106 || item.floorOrder == 113 || item.floorOrder == 120 || item.floorOrder == 126 || item.floorOrder == 470 || item.floorOrder == 472 || item.floorOrder == 474|| item.floorOrder == 480 || item.floorOrder == 484 || item.floorOrder == 486 || item.floorOrder == 493|| item.floorId == 8407|| item.floorId == 497){
       return true;
     }
     return false;
