a6ccc34508d3bcf13076119dea78779c72c52765
linuszlx <linuszhang@126.com>
2024-04-05 20:09:54 +0800

    Update 12306.js

--- 12306/12306.js
+++ 12306/12306.js
@@ -1,15 +1,4 @@
-// 2023-02-22 18:20
-
-let body;
-let obj = JSON.parse($request.body);
-
-if (obj.placementNo === "0007") {
-  body =
-    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
-} else if (obj.placementNo === "G0054") {
-  body = '{"code":"00","materialsList":[]}';
-} else {
-  body = '{"code":"00","message":"无广告返回"}';
-}
-
-$done({ body });
+let body = JSON.parse($response.body);
+if(body.hasOwnProperty('materialsList')){body['materialsList'] = { }};
+if(body.hasOwnProperty('materialsList')){body['advertParam']['skipTime'] = 1};
+$done({body: JSON.stringify(body)});
