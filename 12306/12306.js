44704ec3edc1bccd1ee5a661ba091c590a33f61c
linuszlx <linuszhang@126.com>
2024-04-05 20:19:25 +0800

    Update 12306.js

--- 12306/12306.js
+++ 12306/12306.js
@@ -1,4 +1,4 @@
 let body = JSON.parse($response.body);
-if(body.hasOwnProperty('materialsList')){body['materialsList'] = { }};
+if(body.hasOwnProperty('materialsList')){body['materialsList'] = [ ]};
 if(body.hasOwnProperty('materialsList')){body['advertParam']['skipTime'] = 1};
 $done({body: JSON.stringify(body)});
