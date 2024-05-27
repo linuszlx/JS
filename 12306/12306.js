c872c70e607bbef4d50e1cb0ee997842aade446e
linuszlx <linuszhang@126.com>
2024-05-27 15:13:51 +0800

    Update 12306.js

--- 12306/12306.js
+++ 12306/12306.js
@@ -1,4 +1,20 @@
-let body = JSON.parse($response.body);
-if(body.hasOwnProperty('materialsList')){body['materialsList'] = [ ]};
-if(body.hasOwnProperty('materialsList')){body['advertParam']['skipTime'] = 1};
-$done({body: JSON.stringify(body)});
+// 2024-02-19 10:45
+
+let body = "";
+let obj = JSON.parse($request.body);
+const isQuanX = typeof $task !== "undefined";
+
+if (obj.placementNo === "0007") {
+  body =
+    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+} else if (obj.placementNo === "G0054") {
+  body = '{"code":"00","materialsList":[]}';
+} else {
+  body = '{"code":"00","message":"无广告返回"}';
+}
+
+if (isQuanX) {
+  $done({ body });
+} else {
+  $done({ response: { body } });
+}
