efb7c20c553b045840fbf5caeeb8e41782c6e1e3
linuszlx <linuszhang@126.com>
2024-12-09 09:37:08 +0800

    Update Dianping.js

--- Dianping/Dianping.js
+++ Dianping/Dianping.js
@@ -3,15 +3,17 @@ Dianping Remove Ads - Version 1.0
 Please note that you may need to reinstall app for script to work.
 
 QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/D/大众点评/rewrite/dianPing.conf
 
 ********************************/
 
 const url = $request.url;
 const header = $request.headers;
 const resp = {};
-const headopt = header["M-SHARK-TRACEID"] || header["m-shark-traceid"];
+const traceKey = Object.keys(header).find(key => /^m-(shark-)?traceid$/i.test(key));
+const headopt = traceKey ? header[traceKey] : null;
 
-if (headopt != null) {
+if (headopt) {
   $done({ body:"", headers:"", status: "HTTP/1.1 204 No Content" });
 } else{
   $done({});
