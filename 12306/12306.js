a77aa939997cfb56dc89f1ff0e9c48e73ff0cf5d
linuszlx <linuszhang@126.com>
2024-12-15 22:38:29 +0800

    Update 12306.js

--- 12306/12306.js
+++ 12306/12306.js
@@ -1,38 +1,23 @@
-/***********************************************
+/*
+引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/12306.js
+*/
+// 2024-02-19 10:45
 
-> 应用名称：墨鱼自用12306去广告脚本
-> 脚本作者：@ddgksf2013
-> 微信账号：墨鱼手记
-> 更新时间：2024-08-24
-> 通知频道：https://t.me/ddgksf2021
-> 贡献投稿：https://t.me/ddgksf2013_bot
-> 问题反馈：ddgksf2013@163.com
-> 特别提醒：如需转载请注明出处，谢谢合作！
-> 脚本说明：目前脚本是最简洁也是最完美的了（低调使用），Enjoy！
-> 使用说明：请在本地添加分流 host, ad.12306.cn, direct
+let body = "";
+let obj = JSON.parse($request.body);
+const isQuanX = typeof $task !== "undefined";
 
-[rewrite_local]
+if (obj.placementNo === "0007") {
+  body =
+    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+} else if (obj.placementNo === "G0054") {
+  body = '{"code":"00","materialsList":[]}';
+} else {
+  body = '{"code":"00","message":"无广告返回"}';
+}
 
-^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-analyze-echo-response https://github.com/ddgksf2013/Scripts/raw/master/12306.js
-
-[mitm]
-
-hostname = ad.12306.cn
-
-***********************************************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-const version = 'V1.0.23';
-
-var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},"undefined"!=typeof $task?$done({body:JSON.stringify(ddgksf2013)}):$done({response:{body:JSON.stringify(ddgksf2013)}});
+if (isQuanX) {
+  $done({ body });
+} else {
+  $done({ response: { body } });
+}
