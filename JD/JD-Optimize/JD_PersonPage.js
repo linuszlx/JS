7e9260c78696c7ef748c25f87db2c37eff493fab
linuszlx <linuszhang@126.com>
2024-12-22 16:01:03 +0800

    Update JD_PersonPage.js

--- JD/JD-Optimize/JD_PersonPage.js
+++ JD/JD-Optimize/JD_PersonPage.js
@@ -1,4 +1,5 @@
 let body = JSON.parse($response.body);
+if (body?.floors?.length > 0) {
   body.floors = body.floors.filter(function(item) {
     if (item.mId == "basefloorinfo" || item.mId == "recommendfloor"|| item.mId == "horizontalCard" || item.mId == "buyOften" ) {
       return false;
@@ -7,4 +8,5 @@ let body = JSON.parse($response.body);
   });
 if(body.floors.hasOwnProperty('1')){body['floors']['1']['data']['logisticsInfo'] = { }};
 if(body.floors.hasOwnProperty('1')){body['floors']['1']['data']['commentRemindInfo'] = { }};
+  }
 $done({body: JSON.stringify(body)});
