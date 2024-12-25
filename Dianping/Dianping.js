4ebdf2b304728e875aa63baae82e3a9c8b965e88
linuszlx <linuszhang@126.com>
2024-12-25 16:57:59 +0800

    Update Dianping.js

--- Dianping/Dianping.js
+++ Dianping/Dianping.js
@@ -1,12 +1,18 @@
+/********************************
+Dianping Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+
+********************************/
+
 const url = $request.url;
-const headers = $request.headers;
-const headopt = headers["M-SHARK-TRACEID"] || headers["m-shark-traceid"];
-let body = ""; // 根据你的需求填充 `body` 内容
+const header = $request.headers;
+const resp = {};
+const headopt = header["M-SHARK-TRACEID"] || header["m-shark-traceid"];
 
 if (headopt != null) {
-  // 如果有指定的 header，返回 204 No Content 响应
-  $done({ status: "HTTP/1.1 204 No Content", body: "", headers: {} });
-} else {
-  // 如果没有指定的 header，返回自定义的响应 body
-  $done({ response: { body } });
+  $done({ body:"", headers:"", status: "HTTP/1.1 204 No Content" });
+} else{
+  $done({});
 }
