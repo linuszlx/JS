ac06aef6e0ee77b049805e95bba253c2bbd65fe0
linuszlx <linuszhang@126.com>
2024-12-28 21:00:45 +0800

    Update KFC.js

--- KFC/KFC.js
+++ KFC/KFC.js
@@ -1,9 +1,11 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-obj.data.app6_bootStrapAd = {
-  };
-obj.data.app6_secondFloor = {
-  };
-obj.data.app6_atmosphere = {
-  };
-$done({body: JSON.stringify(obj)});
+var body = $response.body;  // 获取响应体
+var obj = JSON.parse(body);  // 将响应体转换为 JSON 对象
+
+// 检查 obj.data 是否存在，并且包含需要清空的字段
+if (obj.data) {
+  obj.data.app6_bootStrapAd = {};  // 清空 app6_bootStrapAd 字段
+  obj.data.app6_secondFloor = {};  // 清空 app6_secondFloor 字段
+  obj.data.app6_atmosphere = {};   // 清空 app6_atmosphere 字段
+}
+
+$done({body: JSON.stringify(obj)});  // 返回修改后的响应体
