98f0ccfc14b262e4f6c518f703e6b5a21c70e4c4
linuszlx <linuszhang@126.com>
2025-01-05 19:08:12 +0800

    Update PinDuoDuo_DNS.js

--- PinDuoDuo/PinDuoDuo_DNS.js
+++ PinDuoDuo/PinDuoDuo_DNS.js
@@ -1,5 +1,5 @@
-if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 if (request.body && request.body.length > 0) {
-  obj.host_list=[  ] } ;
+  if (obj?.host_list){
+  obj.host_list=[  ] ;} };
 $done({ body: JSON.stringify(obj) });
