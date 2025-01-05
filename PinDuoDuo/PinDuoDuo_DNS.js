a5e6fad175b6d0f0d086714ce6031d76f1f02798
linuszlx <linuszhang@126.com>
2025-01-05 19:13:46 +0800

    Update PinDuoDuo_DNS.js

--- PinDuoDuo/PinDuoDuo_DNS.js
+++ PinDuoDuo/PinDuoDuo_DNS.js
@@ -1,5 +1,12 @@
 let obj = JSON.parse($response.body);
+console.log("原始响应体:", obj); // 查看原始响应体
+
 if (request.body && request.body.length > 0) {
-  if (obj?.host_list){
-  obj.host_list=[  ] ;} };
+  if (obj?.host_list) {
+    console.log("清空 host_list");
+    obj.host_list = [];
+  }
+};
+
+console.log("修改后的响应体:", obj); // 查看修改后的响应体
 $done({ body: JSON.stringify(obj) });
